0aafbf7e792ec2251b7c82b1bb690401b4d8bcf1
Kainzor <sigfus.mar@outlook.com>
2024-11-02 17:05:12 +0000

    fixing error

--- Terraform/Create-AZ-Virtual-Network.tf
+++ Terraform/Create-AZ-Virtual-Network.tf
@@ -28,7 +28,7 @@ resource "azurerm_virtual_network" "test-vn" {
     name = "Kain-terraform-vn"
     resource_group_name = azurerm_resource_group.kaintest-rg.name
     location = azurerm_resource_group.kaintest-rg.location
-    address_space = [ 10.123.0.0/16 ]
+    address_space = ["10.123.1.1/16"]
 
     tags = {
       environment = "dev"
