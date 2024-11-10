aa836ad646ebcda5588360023b8933b27d3b0e9a
Kainzor <sigfus.mar@outlook.com>
2024-11-10 11:49:46 +0000

    Changes to github

---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+admin_users  = ["test@domain.onmicrosoft.com"]
+reader_users = ["test@domain.onmicrosoft.com"]
+
+location = "West Europe"
+
+owner_username = "test@domain.onmicrosoft.com"
---
+++ Terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "admin_users" {
+  type = list(string)
+}
+variable "reader_users" {
+  type = list(string)
+}
+
+variable "location" {
+  type = string
+}
+
+variable "owner_username" {
+  type = string
+
+}
